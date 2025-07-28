ff6bd414f09ea29be6a82c84b713894c4801134a
terrminatorx4 <terrminatorx4@gmail.com>
2025-07-28 22:28:15 +0200

    ⚙️ PWA: Add basic service worker for offline support

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+// BLONDE PLACE PWA Service Worker
+const CACHE_NAME = 'blondeplace-v1';
+const urlsToCache = [
+  '/',
+  '/blog/',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/favicon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
